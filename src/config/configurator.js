bc3b2e63656bb4f892ec024f6c136e74fb0a66c3
Daniel Silva Rocha <danielddsilva2@hotmail.com>
2022-04-06 22:22:23 -0300

    small changes

--- src/config/configurator.js
+++ src/config/configurator.js
@@ -1,37 +1,42 @@
 var misc = require("./misc");
 var isType = require("./types");
 var eventEmitter = require("events");
+
 class Configurator extends eventEmitter{
     #configs  = {};
     #values = {};
     #override = false;
+    #container = {};
     constructor(){
         super();
     };
     putConfig(obj){
         if(typeof obj != "object"){
-            throw misc.generateError("invalid_type",[{},obj,"arg obj"]).message;
+            throw misc.generateError("invalid_type",[{},obj,"arg obj"]);
         }
         if(Array.isArray(obj)){
             obj.forEach(value =>this.putConfig(value));
+            return;
         };
         let name = obj.name;
         let type = obj.type || "any";
         let on_active = obj.on_active || function(){};
-        let isSugar = obj.sugar;
+        let def = obj.def;
         if(this.#configs[name] && !this.#override){
             throw "Config already registred"
         };
-        this.#configs[name] = {name,type,on_active,isSugar}
+        this.#configs[name] = {name,type,on_active,def}
+        if(def != undefined)
+            this.set(name,def);
     };
-    putObjectReader(obj){
+    readJson(obj){
     };
-		getConfig(configName){
-			if(!this.#configs[configName]){
-				return false;
-			}
-			return Object.assign({},this.#configs[configName]);
-		};
+    getConfig(configName){
+        if(!this.#configs[configName]){
+            return false;
+        }
+        return Object.assign({},this.#configs[configName]);
+    };
     get override(){
         return this.#override;
     };
@@ -43,17 +48,17 @@ class Configurator extends eventEmitter{
         }
     }
     set(configName,value){
-			let config = this.#configs[configName];
-			if(!config){
-				throw "invalid config";
-			}
-			let type = config.type;
-			if(isType(type,value)){
-				this.#values[configName] = value;
-			}
+        let config = this.#configs[configName];
+        if(!config){
+            throw "invalid config";
+        }
+        let type = config.type;
+        if(isType(type,value)){
+            this.#values[configName] = value;
+        }
     };
     get(config){
-
+        return this.#values[config];
     };
 }
 
